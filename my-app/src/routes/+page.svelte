cd2e79b44630577567752124b8477e461dc5be7a
BenCampbellNelson <campbellnelsonb@gmail.com>
2023-10-20 21:27:07 +1300

    Initial commit

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+
+    let character_total = 0;
+
+    const BASE_URL = "https://rickandmortyapi.com"
+
+    fetch(BASE_URL + "/api/character")
+    .then((res) => res.json())
+    .then((data) => {
+
+        character_total = data.info.count
+        console.log(character_total)
+    })
+</script>
