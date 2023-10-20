3807404a29164b56e728f7abc6fe19e7662f5e66
BenCampbellNelson <campbellnelsonb@gmail.com>
2023-10-21 11:15:27 +1300

    Fetch character using id

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 
     let character_total = 0;
+    let character = [];
+    
 
     const BASE_URL = "https://rickandmortyapi.com"
 
@@ -9,6 +11,18 @@
     .then((data) => {
 
         character_total = data.info.count
-        console.log(character_total)
+        
+        let character_id = [];
+
+        character_id = Math.floor(Math.random() * character_total);
+
+        console.log(character);
+
+        fetch(`${BASE_URL}/api/character/${character_id}`)
+        .then((res) => res.json())
+        .then((data2) => {
+            character = data2;
+            console.log(character);
+        })
     })
 </script>
