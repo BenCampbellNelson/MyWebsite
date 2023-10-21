01d78bb075798c3c489613434c1f147be2388178
BenCampbellNelson <campbellnelsonb@gmail.com>
2023-10-22 12:10:24 +1300

    Create web pages

--- my-app/src/lib/nav.svelte
+++ my-app/src/lib/nav.svelte
@@ -11,13 +11,13 @@
                 <a class="nav_link is-active" href="/"> HOME </a>
             </li>
             <li class="nav_item t-primary-heading">
-                <a class="nav_link is-active" href="/"> TRIVIA </a>
+                <a class="nav_link is-active" href="/trivia"> TRIVIA </a>
             </li>
             <li class="nav_item t-primary-heading">
-                <a class="nav_link is-active" href="/"> CHARACTER GUESS</a>
+                <a class="nav_link is-active" href="/character_guess"> CHARACTER GUESS</a>
             </li>
             <li class="nav_item t-primary-heading">
-                <a class="nav_link is-active" href="/"> EPISODE GUESS </a>
+                <a class="nav_link is-active" href="/episode_guess"> EPISODE GUESS </a>
             </li>
         </ul>
     </div>
---
+++ my-app/src/routes/character_guess/+page.svelte
@@ -0,0 +1,1 @@
+<h1>character guess</h1>
