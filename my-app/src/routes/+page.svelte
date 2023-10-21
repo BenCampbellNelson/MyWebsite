f8f5963c3b8ea0f539d329b1f0ed376e6b4549ee
BenCampbellNelson <campbellnelsonb@gmail.com>
2023-10-21 13:14:20 +1300

    Add form and label input for character guess

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,36 +1,39 @@
 <script>
+	let character_total = 0;
+	let character = [];
 
-    let character_total = 0;
-    let character = [];
-    
+	const BASE_URL = 'https://rickandmortyapi.com';
 
-    const BASE_URL = "https://rickandmortyapi.com"
+	function fetchRandomCharacter() {
+		fetch(BASE_URL + '/api/character')
+			.then((res) => res.json())
+			.then((data) => {
+				character_total = data.info.count;
+				console.log(data);
 
-    function fetchRandomCharacter() {
-    fetch(BASE_URL + "/api/character")
-    .then((res) => res.json())
-    .then((data) => {
+				let character_id = [];
 
-        character_total = data.info.count
-        console.log(data);
-        
-        let character_id = [];
+				character_id = Math.floor(Math.random() * character_total);
 
-        character_id = Math.floor(Math.random() * character_total);
+				console.log(character);
 
-        console.log(character);
+				fetch(`${BASE_URL}/api/character/${character_id}`)
+					.then((res) => res.json())
+					.then((data2) => {
+						character = data2;
+						console.log(character);
+					});
+			});
+	}
 
-        fetch(`${BASE_URL}/api/character/${character_id}`)
-        .then((res) => res.json())
-        .then((data2) => {
-            character = data2;
-            console.log(character);
-        })
-    })
-}
-
-fetchRandomCharacter();
+	fetchRandomCharacter();
 </script>
 
 <h1>{character.name}</h1>
-<img src={character.image} alt = "characterImage">
+<img src={character.image} alt="characterImage" />
+<form on:submit={checkGuess}>
+	<label>
+		Guess the character's name:
+		<input type="text" name="characterName" />
+	</label>
+</form>
