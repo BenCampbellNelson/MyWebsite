70d26a99360a77b5699a83c5a46f22674edfedde
BenCampbellNelson <campbellnelsonb@gmail.com>
2023-10-25 10:02:42 +1300

    Add a bunch of lines fo rtesting footer

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -3,6 +3,26 @@
 <div class="page-title">
     <div class="page-title_inner">
         <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        <h1 class="page-title t-title">HOME PAGE</h1>
+        
     </div>
 
 </div>
