d18baa1b2f174cab33e88420946048e832c1a216
BenCampbellNelson <campbellnelsonb@gmail.com>
2023-10-24 21:29:56 +1300

    reset the input field once guess is submitted

--- my-app/src/routes/character_guess/+page.svelte
+++ my-app/src/routes/character_guess/+page.svelte
@@ -1,53 +1,56 @@
 <script>
-    
-	let character_total = 0;
-	let character = [];
-
-	const BASE_URL = 'https://rickandmortyapi.com';
-
-	function fetchRandomCharacter() {
-		fetch(BASE_URL + '/api/character')
-			.then((res) => res.json())
-			.then((data) => {
-				character_total = data.info.count;
-				console.log(data);
-
-				let character_id = [];
-
-				character_id = Math.floor(Math.random() * character_total);
-
-				console.log(character);
-
-				fetch(`${BASE_URL}/api/character/${character_id}`)
-					.then((res) => res.json())
-					.then((data2) => {
-						character = data2;
-						console.log(character);
-					});
-			});
-	}
-
-    function checkGuess(event) {
-        event.preventDefault();
-        const guess = event.target.querySelector('input[name="characterName"]').value;
-        if (character.name.toLowerCase() === guess.toLowerCase()) {
-            
-            fetchRandomCharacter();
-        } else {
-            
-            alert("Incorrect guess, please try again.");
-        }
+    let character_total = 0;
+    let character = [];
+    let guessInput = ''; // Added variable to store the input field value
+  
+    const BASE_URL = 'https://rickandmortyapi.com';
+  
+    function fetchRandomCharacter() {
+      fetch(BASE_URL + '/api/character')
+        .then((res) => res.json())
+        .then((data) => {
+          character_total = data.info.count;
+          console.log(data);
+  
+          let character_id = [];
+  
+          character_id = Math.floor(Math.random() * character_total);
+  
+          console.log(character);
+  
+          fetch(`${BASE_URL}/api/character/${character_id}`)
+            .then((res) => res.json())
+            .then((data2) => {
+              character = data2;
+              console.log(character);
+            });
+        });
     }
-
-	fetchRandomCharacter();
-</script>
-
-<h1>{character.name}</h1>
-<img src={character.image} alt="characterImage" />
-<form on:submit={checkGuess}>
-	<label>
-		Guess the character's name:
-		<input type="text" name="characterName" />
-	</label>
-    <button type ="submit">Submit Guess</button>
-</form>
+  
+    function checkGuess(event) {
+      event.preventDefault();
+      const guess = event.target.querySelector('input[name="characterName"]').value;
+      if (character.name.toLowerCase() === guess.toLowerCase()) {
+        fetchRandomCharacter();
+        guessInput = ''; // Reset the input field value
+      } else {
+        alert('Incorrect guess, please try again.');
+        guessInput = '';
+      }
+    }
+  
+    fetchRandomCharacter();
+  </script>
+  
+  <div class="characterGuess">
+    <h1>{character.name}</h1>
+    <img src={character.image} alt="characterImage" />
+    <form on:submit={checkGuess}>
+      <label>
+        Guess the character's name:
+        <input type="text" name="characterName" bind:value={guessInput} />
+      </label>
+      <button type="submit">Submit Guess</button>
+    </form>
+  </div>
+  
