5f6993d3c321642c46433ad3cb34fe021beb0058
BenCampbellNelson <campbellnelsonb@gmail.com>
2023-10-25 10:11:10 +1300

    Create structure of footer

--- my-app/src/lib/footer.svelte
+++ my-app/src/lib/footer.svelte
@@ -2,6 +2,15 @@
 
 <div class ="footer">
     <h1>this is the footer</h1>
+    <div class="footerNav">
+        nav
+    </div>
+    <div class ="socialMedia">
+        social
+    </div>
+    <div class = "copyRight">
+        copyright
+    </div>
 </div>
 
 
@@ -9,6 +18,8 @@
     .footer{
         display: flex;
         margin-top: auto;
-
+        flex-direction: column;
+        background-color: #262c3a;
+        color: white;
     }
 </style>
