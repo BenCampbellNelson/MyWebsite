40816bc15354c1fe034d083302a8a667701001db
BenCampbellNelson <campbellnelsonb@gmail.com>
2023-10-25 10:02:18 +1300

    Stick footer to bottom of page

--- my-app/src/lib/footer.svelte
+++ my-app/src/lib/footer.svelte
@@ -1,1 +1,14 @@
-<h1>this is the footer</h1>
+
+
+<div class ="footer">
+    <h1>this is the footer</h1>
+</div>
+
+
+<style>
+    .footer{
+        display: flex;
+        margin-top: auto;
+
+    }
+</style>
