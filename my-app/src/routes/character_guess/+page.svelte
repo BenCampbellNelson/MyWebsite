c452d1c9a01ed121fa7e5e76687ad1d05e413274
BenCampbellNelson <campbellnelsonb@gmail.com>
2023-10-26 20:03:10 +1300

    Add incorrect guess text to character guess

--- my-app/src/routes/character_guess/+page.svelte
+++ my-app/src/routes/character_guess/+page.svelte
@@ -1,56 +1,71 @@
 <script>
-    let character_total = 0;
-    let character = [];
-    let guessInput = ''; // Added variable to store the input field value
-  
-    const BASE_URL = 'https://rickandmortyapi.com';
-  
-    function fetchRandomCharacter() {
-      fetch(BASE_URL + '/api/character')
-        .then((res) => res.json())
-        .then((data) => {
-          character_total = data.info.count;
-          console.log(data);
-  
-          let character_id = [];
-  
-          character_id = Math.floor(Math.random() * character_total);
-  
-          console.log(character);
-  
-          fetch(`${BASE_URL}/api/character/${character_id}`)
-            .then((res) => res.json())
-            .then((data2) => {
-              character = data2;
-              console.log(character);
-            });
-        });
-    }
-  
-    function checkGuess(event) {
-      event.preventDefault();
-      const guess = event.target.querySelector('input[name="characterName"]').value;
-      if (character.name.toLowerCase() === guess.toLowerCase()) {
-        fetchRandomCharacter();
-        guessInput = ''; // Reset the input field value
-      } else {
-        alert('Incorrect guess, please try again.');
-        guessInput = '';
-      }
-    }
-  
-    fetchRandomCharacter();
-  </script>
-  
-  <div class="characterGuess">
-    <h1>{character.name}</h1>
-    <img src={character.image} alt="characterImage" />
-    <form on:submit={checkGuess}>
-      <label>
-        Guess the character's name:
-        <input type="text" name="characterName" bind:value={guessInput} />
-      </label>
-      <button type="submit">Submit Guess</button>
-    </form>
-  </div>
-  
+	let character_total = 0;
+	let character = [];
+	let guessInput = ''; // Added variable to store the input field value
+
+	const BASE_URL = 'https://rickandmortyapi.com';
+
+	function fetchRandomCharacter() {
+		fetch(BASE_URL + '/api/character')
+			.then((res) => res.json())
+			.then((data) => {
+				character_total = data.info.count;
+				console.log(data);
+
+				let character_id = [];
+
+				character_id = Math.floor(Math.random() * character_total);
+
+				console.log(character);
+
+				fetch(`${BASE_URL}/api/character/${character_id}`)
+					.then((res) => res.json())
+					.then((data2) => {
+						character = data2;
+						console.log(character);
+					});
+			});
+	}
+
+	function checkGuess(event) {
+		event.preventDefault();
+		const guess = event.target.querySelector('input[name="characterName"]').value;
+		const messageElement = document.getElementById('message');
+
+		if (character.name.toLowerCase() === guess.toLowerCase()) {
+			fetchRandomCharacter();
+			guessInput = ''; // Reset the input field value
+		} else {
+			// Display an incorrect guess message
+			messageElement.textContent = 'Incorrect guess, please try again.';
+			messageElement.style.display = 'block';
+
+			// Hide the message after a few seconds (e.g., 3 seconds)
+			setTimeout(() => {
+				messageElement.style.display = 'none';
+			}, 3000);
+
+			guessInput = '';
+		}
+	}
+
+	fetchRandomCharacter();
+</script>
+
+<div class="pageContainer">
+	<div class="characterGuess">
+		<h1>{character.name}</h1>
+		<img src={character.image} alt="characterImage" />
+		<form on:submit={checkGuess}>
+			<label>
+				Guess the character's name:
+				<input type="text" name="characterName" bind:value={guessInput} />
+			</label>
+			<button type="submit">Submit Guess</button>
+		</form>
+		<div id="message" style="display: none;"></div> <!-- Message element -->
+	</div>
+</div>
+
+
+
