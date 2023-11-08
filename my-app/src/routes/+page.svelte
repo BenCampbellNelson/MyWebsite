fc60898f78daeff3166b4aabfef12f52442b5371
BenCampbellNelson <campbellnelsonb@gmail.com>
2023-11-08 13:02:03 +1300

    Remove some silly nesting

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,32 +1,39 @@
 
 <div class="page-title">
     <div class="page-title_inner">
-        <div class="content-box">
-            <div class="flex-box">
+        
+            <div class="content-box">
                 <div class="image">
                     <img src="images/RickandMortyPortal.jpg" alt="image">
                 </div>
                 <div class="text">
-                    <h1>placement text</h1>
+                    <h2>Mark Justin Roiland was a writer, animator, director, producer and the co-creator of Rick and Morty as well as the voice of Rick and Morty from Season 1 to Season 6.
+
+                        As of January 24, 2023, he has been dismissed from the series amid charges of domestic abuse and his voice roles will be recast with soundalike actors starting with Season 7.[1][2]
+                        
+                        Following his firing, the charges had been dismissed for insufficient evidence, but allegations of sexual assault were reported against him.</h2>
                 </div>
             </div>
-        </div>
-        <div class="content-box">
-            <div class="flex-box alternate">
+    
+       
+            <div class="content-box">
                 <div class="text">
-                   <h1>placement text</h1>
+                   <h2>Dan Harmon is a writer, producer, actor and the co-creator of Rick and Morty as well as an executive producer for the series.
+
+                    As of January 24, 2023, he is the sole showrunner of the series, as Adult Swim severed its ties with co-creator Justin Roiland and fired him from the series amid charges of domestic abuse.[1]</h2>
                 </div>
                 <div class="image">
                     <img src="images/RickandMortyPortal.jpg" alt="image">
                 </div>
             </div>
-        </div>
+        
     </div>
 </div>
 
 <style>
 
-    .flex-box{
+    .content-box{
         display: flex;
     }
+
 </style>
