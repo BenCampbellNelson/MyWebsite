3e4d25930acf32031ed3cd11a40c89f93d9fde6e
BenCampbellNelson <campbellnelsonb@gmail.com>
2023-11-08 16:40:07 +1300

    Add background image to layour so is on every page

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -6,7 +6,19 @@
 </script>
 
 <Header />
-
+<div class="backgroundImage"></div>
     <slot />
-
 <Footer />
+
+<style>
+     .backgroundImage{
+		background-image: url('/images/RickInTheCorner.jpg');
+		background-size: cover;
+		background-repeat: no-repeat;
+        position: absolute;
+        z-index: -12;
+        width: 100vw;
+        height: 100vh;
+        background-attachment: fixed; /* Add this line */
+	}
+</style>
