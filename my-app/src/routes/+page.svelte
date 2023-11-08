dfaed588120fed1dd0070fae6dd6485c68a83c53
BenCampbellNelson <campbellnelsonb@gmail.com>
2023-11-08 14:18:12 +1300

    Make home page resposnive

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,39 +1,119 @@
-
 <div class="page-title">
-    <div class="page-title_inner">
-        
-            <div class="content-box">
-                <div class="image">
-                    <img src="images/RickandMortyPortal.jpg" alt="image">
-                </div>
-                <div class="text">
-                    <h2>Mark Justin Roiland was a writer, animator, director, producer and the co-creator of Rick and Morty as well as the voice of Rick and Morty from Season 1 to Season 6.
+	<div class="page-title-inner">
+		<div class="content-box">
+			<div class="image">
+				<img src="images/justinRoiland.webp" alt="image" />
+			</div>
+			<div class="text">
+				<h2>
+					Mark Justin Roiland was a writer, animator, director, producer and the co-creator of Rick
+					and Morty as well as the voice of Rick and Morty from Season 1 to Season 6. As of January
+					24, 2023, he has been dismissed from the series amid charges of domestic abuse and his
+					voice roles will be recast with soundalike actors starting with Season 7.[1][2] Following
+					his firing, the charges had been dismissed for insufficient evidence, but allegations of
+					sexual assault were reported against him.
+				</h2>
+			</div>
+		</div>
 
-                        As of January 24, 2023, he has been dismissed from the series amid charges of domestic abuse and his voice roles will be recast with soundalike actors starting with Season 7.[1][2]
-                        
-                        Following his firing, the charges had been dismissed for insufficient evidence, but allegations of sexual assault were reported against him.</h2>
-                </div>
-            </div>
-    
-       
-            <div class="content-box">
-                <div class="text">
-                   <h2>Dan Harmon is a writer, producer, actor and the co-creator of Rick and Morty as well as an executive producer for the series.
+		<div class="content-box">
+			<div class="text">
+				<h2>
+					Dan Harmon is a writer, producer, actor and the co-creator of Rick and Morty as well as an
+					executive producer for the series. As of January 24, 2023, he is the sole showrunner of
+					the series, as Adult Swim severed its ties with co-creator Justin Roiland and fired him
+					from the series amid charges of domestic abuse.[1]
+				</h2>
+			</div>
+			<div class="image">
+				<img src="images/RickandMortyPortal.jpg" alt="image" />
+			</div>
+		</div>
 
-                    As of January 24, 2023, he is the sole showrunner of the series, as Adult Swim severed its ties with co-creator Justin Roiland and fired him from the series amid charges of domestic abuse.[1]</h2>
-                </div>
-                <div class="image">
-                    <img src="images/RickandMortyPortal.jpg" alt="image">
-                </div>
-            </div>
-        
-    </div>
+		<div class="content-box">
+			<div class="image">
+				<img src="images/RickandMortyPortal.jpg" alt="image" />
+			</div>
+			<div class="text">
+				<h2>
+					Mark Justin Roiland was a writer, animator, director, producer and the co-creator of Rick
+					and Morty as well as the voice of Rick and Morty from Season 1 to Season 6. As of January
+					24, 2023, he has been dismissed from the series amid charges of domestic abuse and his
+					voice roles will be recast with soundalike actors starting with Season 7.[1][2] Following
+					his firing, the charges had been dismissed for insufficient evidence, but allegations of
+					sexual assault were reported against him.
+				</h2>
+			</div>
+		</div>
+
+		<div class="content-box">
+			<div class="text">
+				<h2>
+					Dan Harmon is a writer, producer, actor and the co-creator of Rick and Morty as well as an
+					executive producer for the series. As of January 24, 2023, he is the sole showrunner of
+					the series, as Adult Swim severed its ties with co-creator Justin Roiland and fired him
+					from the series amid charges of domestic abuse.[1]
+				</h2>
+			</div>
+			<div class="image">
+				<img src="images/justinRoiland.webp" alt="image" />
+			</div>
+		</div>
+	</div>
 </div>
 
 <style>
+	.content-box {
+		display: flex;
+		width: 100%;
+		border-color: black;
+		border-style: dashed;
+		padding: 10px;
+	}
 
-    .content-box{
-        display: flex;
-    }
+	.image {
+		flex: 0 0 600px;
+		height: 600px;
+		overflow: hidden;
+	}
 
+	.image img {
+		width: 100%;
+		height: 100%;
+	}
+	.text {
+		flex: 1;
+	}
+
+	.page-title-inner {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		max-width: 80%;
+		margin: 0 auto;
+		padding: 20px;
+	}
+
+	@media (max-width: 1200px) {
+		.image {
+			flex: 0 0 300px;
+			height: 300px;
+			overflow: hidden;
+            align-items: center;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+		}
+
+		.content-box {
+			display: flex;
+			flex-direction: column;
+		}
+
+        .image img {
+		width: 50%; 
+		height: 100%;
+	}
+	}
 </style>
