2e596661cf0d10fb258080d7a4a3af18a5a2624d
BenCampbellNelson <campbellnelsonb@gmail.com>
2023-11-08 17:45:28 +1300

    Fix image getting cut off on scroll so now is fixed at back

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,24 +1,27 @@
 <script>
-
-	import Footer from '../lib/footer.svelte';
+    import Footer from '../lib/footer.svelte';
     import Header from '../lib/header.svelte';
-
 </script>
 
 <Header />
 <div class="backgroundImage"></div>
+
     <slot />
+
 <Footer />
 
 <style>
-     .backgroundImage{
-		background-image: url('/images/RickInTheCorner.jpg');
-		background-size: cover;
-		background-repeat: no-repeat;
-        position: absolute;
-        z-index: -12;
-        width: 100vw;
-        height: 100vh;
-        background-attachment: fixed; /* Add this line */
-	}
+.backgroundImage {
+    background-image: url('/images/RickInTheCorner.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    position: fixed; 
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1; 
+}
+
 </style>
