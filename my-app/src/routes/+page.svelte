ac1cf9b60d575bad4562c8c9c9bdc916bd53bdae
BenCampbellNelson <campbellnelsonb@gmail.com>
2023-11-08 17:45:40 +1300

    ..

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -35,6 +35,78 @@
         </div>
     </div>
 </div>
+
+<div class="card">
+    <div class="content">
+        <div class="image">
+            <img src="images/justinRoiland.webp" alt="Justin Roiland" />
+        </div>
+        <div class="text">
+            <h2>Mark Justin Roiland</h2>
+            <p>
+                Mark Justin Roiland is a writer, animator, director, producer, and the co-creator of the
+                popular show "Rick and Morty." He provided the voices for Rick and Morty from Season 1 to
+                Season 6. However, as of January 24, 2023, he has been dismissed from the series amid
+                charges of domestic abuse. His voice roles will be recast with soundalike actors starting with
+                Season 7. It's important to note that following his firing, the charges were dismissed due
+                to insufficient evidence, but allegations of sexual assault were reported against him.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="card">
+    <div class="content">
+        <div class="text">
+            <h2>Dan Harmon</h2>
+            <p>
+                Dan Harmon is a writer, producer, actor, and the co-creator of "Rick and Morty." He has also
+                served as an executive producer for the series. As of January 24, 2023, he is the sole
+                showrunner of the series. This decision was made by Adult Swim after severing ties with
+                co-creator Justin Roiland, who was fired from the series amid charges of domestic abuse.
+            </p>
+        </div>
+        <div class="image">
+            <img src="images/Dan_Harmon1.webp" alt="Dan Harmon" />
+        </div>
+    </div>
+</div>
+
+<div class="card">
+    <div class="content">
+        <div class="image">
+            <img src="images/justinRoiland.webp" alt="Justin Roiland" />
+        </div>
+        <div class="text">
+            <h2>Mark Justin Roiland</h2>
+            <p>
+                Mark Justin Roiland is a writer, animator, director, producer, and the co-creator of the
+                popular show "Rick and Morty." He provided the voices for Rick and Morty from Season 1 to
+                Season 6. However, as of January 24, 2023, he has been dismissed from the series amid
+                charges of domestic abuse. His voice roles will be recast with soundalike actors starting with
+                Season 7. It's important to note that following his firing, the charges were dismissed due
+                to insufficient evidence, but allegations of sexual assault were reported against him.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="card">
+    <div class="content">
+        <div class="text">
+            <h2>Dan Harmon</h2>
+            <p>
+                Dan Harmon is a writer, producer, actor, and the co-creator of "Rick and Morty." He has also
+                served as an executive producer for the series. As of January 24, 2023, he is the sole
+                showrunner of the series. This decision was made by Adult Swim after severing ties with
+                co-creator Justin Roiland, who was fired from the series amid charges of domestic abuse.
+            </p>
+        </div>
+        <div class="image">
+            <img src="images/Dan_Harmon1.webp" alt="Dan Harmon" />
+        </div>
+    </div>
+</div>
 </div>
 <style>
     .card {
